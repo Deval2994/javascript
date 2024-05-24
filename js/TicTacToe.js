c7ba0a20a2a69f740190aa6f7ec456ed9e43cb4c
deval <deval.patel5@dcmail.ca>
2024-05-24 16:54:17 -0400

    player's alternative chance set

--- js/TicTacToe.js
+++ js/TicTacToe.js
@@ -1,11 +1,17 @@
-const x = "X";
-const o = "O";
 
-const box = document.querySelectorAll("#box")
-
-box.forEach(btn => {
-    btn.onclick = function(){
-        console.log(x)
-        console.log(o)
-    }
+const boxes = document.querySelectorAll("#box")
+let i = 1;
+boxes.forEach((box) => {
+    box.addEventListener("click", () => {
+        if (i %2 === 0){
+            box.textContent = "O"
+            box.style.color = "rgb(0, 255, 0)"
+            box.style.textShadow = "0 0 1rem rgba(0, 255, 0, 0.75)"
+        }else{
+            box.textContent = "X"
+            box.style.color = "rgb(255, 0, 0)"
+            box.style.textShadow = "0 0 1rem rgba(255, 0, 0, 0.75)"
+        }
+        i++;
+    })
 })
